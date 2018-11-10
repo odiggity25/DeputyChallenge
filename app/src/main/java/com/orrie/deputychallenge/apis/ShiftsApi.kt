52998552082f2afe35e1c0abb90a05dcc7271153
Orrie <orrie@insight.co>
2018-11-10 22:40:38 +1100

    Added start and end shift apis

--- app/src/main/java/com/orrie/deputychallenge/apis/ShiftsApi.kt
+++ app/src/main/java/com/orrie/deputychallenge/apis/ShiftsApi.kt
@@ -1,12 +1,22 @@
 package com.orrie.deputychallenge.apis
 
 import com.orrie.deputychallenge.models.Shift
+import com.orrie.deputychallenge.models.ShiftChange
+import io.reactivex.Completable
 import io.reactivex.Single
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ShiftsApi {
 
     @GET("/shifts")
     fun getShifts(): Single<List<Shift>>
+
+    @POST("/shift/start")
+    fun startShift(@Body shiftChange: ShiftChange): Completable
+
+    @POST("/shift/end")
+    fun endShift(@Body shiftChange: ShiftChange): Completable
 
 }
--- app/src/main/java/com/orrie/deputychallenge/repositories/ShiftsRepository.kt
+++ app/src/main/java/com/orrie/deputychallenge/repositories/ShiftsRepository.kt
@@ -2,6 +2,8 @@ package com.orrie.deputychallenge.repositories
 
 import com.orrie.deputychallenge.apis.ShiftsApi
 import com.orrie.deputychallenge.models.Shift
+import com.orrie.deputychallenge.models.ShiftChange
+import io.reactivex.Completable
 import io.reactivex.Single
 
 class ShiftsRepository(
@@ -12,5 +14,13 @@ class ShiftsRepository(
 
     fun getShifts(): Single<List<Shift>> {
         return shiftsApi.getShifts()
+    }
+
+    fun startShift(shiftChange: ShiftChange): Completable {
+        return shiftsApi.startShift(shiftChange)
+    }
+
+    fun endShift(shiftChange: ShiftChange): Completable {
+        return shiftsApi.endShift(shiftChange)
     }
 }
