3e9f6ec8725fc9e60df43109165422d576f5eb2e
Orrie <orrie@insight.co>
2018-11-10 20:38:58 +1100

    Initial commit; Includes library dependencies

---
+++ app/src/main/java/com/orrie/deputychallenge/ShiftsActivity.kt
@@ -0,0 +1,12 @@
+package com.orrie.deputychallenge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShiftsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shifts)
+    }
+}
